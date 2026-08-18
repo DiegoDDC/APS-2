44fc3277fd9c8704b6c187cc1ec976c837d5c053
DIEGO DANTAS CAVALCANTI <diego.cavalcanti@cs.unipe.edu.br>
2026-08-17 21:32:06 -0300

    Exercicio feito em sala com construtores, getting, setting

---
+++ exemplo2/src/main/resources/Principal.java
@@ -0,0 +1,10 @@
+public class Principal {
+
+    public static void main(string[] args){
+        Banco b3 = new Banco ("José", "111", 10);
+
+        System.out.println(b3.getNome());
+        b3.setNome("Joaquim");
+        System.out.println(b3.getNome());
+    }
+}
---
+++ exemplo2/src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $br.edu.unipe.br;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
